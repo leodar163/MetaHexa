ccb140af5954164fc982ba09df3bc7f6beff4301
leo BG <leobgely@gmail.com>
2023-06-06 19:50:03 +0200

    avant la suppr

---
+++ Metexa/Assets/Scripts/PlaneteProcedurale/ComputeShaderTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MetaHexa.PlaneteProcedurale
+{
+    public class ComputeShaderTest : MonoBehaviour
+    {
+        [SerializeField] private ComputeShader _cs;
+        [SerializeField] private MeshFilter _meshFilter;
+
+        private struct MeshCS
+        {
+            
+        }
+    }
+}
